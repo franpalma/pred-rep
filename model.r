fd6c0040760682ad26998c0051e86c6653399bc8
FrancisPalma-PC\Francis Palma <francis.palma@polymtl.ca>
2018-06-25 17:15:41 -0400

    no message

---
+++ model.r
@@ -0,0 +1,19 @@
+setwd('D:/pred-rep/tanzeem_noor-promise17_data')
+
+#load data
+rq1_closure_compiler <- read.csv("RQ1_Closure-Compiler.csv", header = TRUE)
+rq1_commons_lang <- read.csv("RQ1_Commons-Lang.csv", header = TRUE)
+rq1_commons_math <- read.csv("RQ1_Commons-Math.csv", header = TRUE)
+rq1_jfreechart <- read.csv("RQ1_JFreeChart.csv", header = TRUE)
+rq1_joda_time <- read.csv("RQ1_Joda-Time.csv", header = TRUE)
+rq2_closure_compiler <- read.csv("RQ2_Closure-Compiler.csv", header = TRUE)
+rq2_commons_lang <- read.csv("RQ2_Commons-Lang.csv", header = TRUE)
+rq2_commons_math <- read.csv("RQ2_Commons-Math.csv", header = TRUE)
+rq2_jfreechart <- read.csv("RQ2_JFreeChart.csv", header = TRUE)
+rq2_joda_time <- read.csv("RQ2_Joda-Time.csv", header = TRUE)
+raw_closure_compiler <- read.csv("Closure-Compiler Metrics Raw_Data.csv", header = TRUE)
+raw_commons_lang <- read.csv("Commons-Lang Metrics Raw_Data.csv", header = TRUE)
+raw_commons_math <- read.csv("Commons-Math Metrics Raw_Data.csv", header = TRUE)
+raw_jfreechart <- read.csv("JFreeChart Metrics Raw_Data.csv", header = TRUE)
+raw_joda_time <- read.csv("Joda-Time Metrics Raw_Data.csv", header = TRUE)
+
